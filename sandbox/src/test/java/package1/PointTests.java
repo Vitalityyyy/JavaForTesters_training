8e3bea0933ee0a0f2c342900728690d9a2d270c4
Vita <vita_f@list.ru>
2022-10-12 00:12:12 +0300

    Добавлен .gitignore, исправлен тест с неправильными данными

--- sandbox/src/test/java/package1/PointTests.java
+++ sandbox/src/test/java/package1/PointTests.java
@@ -24,8 +24,9 @@ public class PointTests {
     public void testDistanceWithIncorrectData() {
         Point p1 = new Point(0.5,8.24);
         Point p2 = new Point(-31.02,0);
+        Point p3 = new Point (20,40)
         double distanceActual = Point.distance(p1, p2);
-        double distanceExpected = Math.sqrt(Math.pow((p1.x-p2.x),2) + Math.pow((p1.y-p2.y),2));
-        Assert.assertFalse(distanceActual!=distanceExpected);
+        double distanceIncorrect = Point.distance(p1, p3);
+        Assert.assertNotEquals(distanceActual,distanceIncorrect);
     }
 }
