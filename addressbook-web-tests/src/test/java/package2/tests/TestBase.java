63141aa8b39fe0780bcf2c0dcec85b978174c90a
Vita <vita_f@list.ru>
2022-10-30 18:52:28 +0300

    Реализована возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/package2/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/package2/tests/TestBase.java
@@ -1,12 +1,13 @@
 package package2.tests;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import package2.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(Browser.CHROME.browserName());
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
