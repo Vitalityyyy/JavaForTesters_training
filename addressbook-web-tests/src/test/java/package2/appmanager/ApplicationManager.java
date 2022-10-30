63ff3f3ac7ece764eabc30e0231f6ad92d105b2a
Vita <vita_f@list.ru>
2022-10-30 20:17:41 +0300

    Оптимизированы заполнение форм ввода и переходы между страницами

--- addressbook-web-tests/src/test/java/package2/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/package2/appmanager/ApplicationManager.java
@@ -32,7 +32,7 @@ public class ApplicationManager {
             driver = new EdgeDriver();
         }
         //System.setProperty("webdriver.gecko.driver", "C:/distr/geckodriver.exe");
-        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/");
         sessionHelper = new SessionHelper(driver);
         groupHelper = new GroupHelper(driver);
--- addressbook-web-tests/src/test/java/package2/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/package2/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase{
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().goToGroupPage();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("Group1", "Header1", "Footer1"));
+        app.getGroupHelper().fillGroupForm(new GroupData("Group1", null, null));
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
     }
