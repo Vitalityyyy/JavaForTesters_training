3a6c02aa798767d73fbf5e20d2c01be91b777492
Vita <vita_f@list.ru>
2022-11-28 23:03:33 +0300

    Реализована проверка путём сравнения списков
    Реализована проверка путём сравнения списков в тестах для создания, удаления и модификации групп и контактов.

--- addressbook-web-tests/src/test/java/package2/model/ContactData.java
+++ addressbook-web-tests/src/test/java/package2/model/ContactData.java
@@ -1,15 +1,25 @@
 package package2.model;
 
+import java.util.Objects;
+
 public class ContactData {
     private final String lastName;
     private final String firstName;
     private final String address;
     private final String email;
     private final String phone;
-
-
+    private int id;
 
     public ContactData(String lastName, String firstName, String address, String email, String phone) {
+        this.id = 0;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+    }
+    public ContactData(int id, String lastName, String firstName, String address, String email, String phone) {
+        this.id = id;
         this.lastName = lastName;
         this.firstName = firstName;
         this.address = address;
@@ -31,5 +41,41 @@ public class ContactData {
     }
     public String getPhone() {
         return phone;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (id != that.id) return false;
+        if (!Objects.equals(lastName, that.lastName)) return false;
+        return Objects.equals(firstName, that.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = lastName != null ? lastName.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + id;
+        return result;
     }
 }
--- addressbook-web-tests/src/test/java/package2/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/package2/tests/GroupModificationTests.java
@@ -1,7 +1,12 @@
 package package2.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import package2.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
     @Test
@@ -10,10 +15,21 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("Group1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Group2", "Header2", "Footer2"));
+        GroupData editedGroup = new GroupData(before.get(before.size()-1).getId(),"Group2", "Header2", "Footer2");
+        app.getGroupHelper().fillGroupForm(editedGroup);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(editedGroup);
+        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
